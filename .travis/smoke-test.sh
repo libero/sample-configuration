aa6d39eb32735a175ae2e4a0dcb38f9d65994227
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-22 16:28:15 +0000

    Use dashes in container names, at least in the service part

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -3,7 +3,7 @@ set -ex
 
 COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-sampleconfiguration}"
 
-.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_dummyapi_fpm_1"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_dummy-api_fpm_1"
 echo "Smoke testing dummy-api"
 [[ "$(curl -sS -H 'Host: unstable--dummy-api.libero.pub' http://localhost:8080/ping 2>&1)" == "pong" ]]
 
