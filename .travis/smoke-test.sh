20297a19b0c5869e78845cef63b5c1a3bfbd0622
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-23 14:22:01 +0000

    Use good default for COMPOSE_PROJECT_NAME rather than Travis's one

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-sampleconfiguration}"
+COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-sample-configuration}"
 HTTP_PORT="${HTTP_PORT:-8080}"
 
 echo "Wait for containers health"
