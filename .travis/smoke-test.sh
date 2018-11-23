6c8a14115be357fd810321784955030c278182c7
Giorgio Sironi <accounts@giorgiosironi.com>
2018-11-23 10:38:15 +0000

    Add healthcheck for nginx (#8)

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -3,11 +3,14 @@ set -ex
 
 COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-sampleconfiguration}"
 
+echo "Wait for containers health"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_dummy-api_fpm_1"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_browser_fpm_1"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_web_1"
+
 echo "Smoke testing dummy-api"
 [[ "$(curl -sS -H 'Host: unstable--dummy-api.libero.pub' http://localhost:8080/ping 2>&1)" == "pong" ]]
 
-.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_browser_fpm_1"
 echo "Smoke testing browser"
 #[[ "$(curl -sS -H 'Host: unstable.libero.pub' http://localhost:8080/articles/42 2>&1)" == "42" ]]
 [[ "$(curl -sS --header 'Host: unstable.libero.pub' http://localhost:8080/favicon.ico --output /dev/null --write-out '%{http_code}')" == "200" ]]
