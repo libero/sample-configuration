224eae9f656d6be39d72f809e1fe7fb19122a857
Chris Wilkinson <chriswilkinson84@gmail.com>
2018-12-18 14:00:22 +0000

    Add Pattern Library (#11)
    * Add Pattern Library
    
    * Set default
    
    * Rename
    
    * Remove healthcheck

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -7,6 +7,7 @@ HTTP_PORT="${HTTP_PORT:-8080}"
 echo "Wait for containers health"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_dummy-api_fpm_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_browser_fpm_1"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_pattern-library_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_web_1"
 
 echo "Smoke testing dummy-api"
@@ -17,3 +18,6 @@ echo "Smoke testing dummy-api"
 echo "Smoke testing browser"
 [[ "$(curl -sS -H 'Host: unstable.libero.pub' "http://localhost:${HTTP_PORT}/blog/post1" --output /dev/null --write-out '%{http_code}')" == "200" ]]
 [[ "$(curl -sS --header 'Host: unstable.libero.pub' "http://localhost:${HTTP_PORT}/favicon.ico" --output /dev/null --write-out '%{http_code}')" == "200" ]]
+
+echo "Smoke testing pattern library"
+[[ "$(curl -sS --header 'Host: unstable--pattern-library.libero.pub' "http://localhost:${HTTP_PORT}" --output /dev/null --write-out '%{http_code}')" == "200" ]]
