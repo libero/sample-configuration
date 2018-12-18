3a60e3e7e36d368005e483d73fe962fd3a42e653
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-12-18 12:14:07 +0000

    Rename

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -7,7 +7,7 @@ HTTP_PORT="${HTTP_PORT:-8080}"
 echo "Wait for containers health"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_dummy-api_fpm_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_browser_fpm_1"
-.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_pattern_library_1"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_pattern-library_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_web_1"
 
 echo "Smoke testing dummy-api"
@@ -20,4 +20,4 @@ echo "Smoke testing browser"
 [[ "$(curl -sS --header 'Host: unstable.libero.pub' "http://localhost:${HTTP_PORT}/favicon.ico" --output /dev/null --write-out '%{http_code}')" == "200" ]]
 
 echo "Smoke testing pattern library"
-[[ "$(curl -sS -H 'Host: unstable--pattern-library.libero.pub' "http://localhost:${HTTP_PORT}" --output /dev/null --write-out '%{http_code}')" == "200" ]]
+[[ "$(curl -sS --header 'Host: unstable--pattern-library.libero.pub' "http://localhost:${HTTP_PORT}" --output /dev/null --write-out '%{http_code}')" == "200" ]]
