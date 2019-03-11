79d6a8700d314b9bc974defc05ee2477529a4804
Giorgio Sironi <accounts@giorgiosironi.com>
2019-03-11 11:42:59 +0000

    Deploy content-store through an api-gateway (#17)
    * Run content-store_fpm service
    
    * Add horrible single nginx configuration
    
    * Remove commented out nginx configuration
    
    * Split api-gateway nginx in proxy and content-store_web
    
    * Include APP_ENV to explore debug logs
    
    * Force a prod environment for content-store
    
    * Update content-store to a version with prod as default
    
    * Remove custom logging configuration
    
    * Add smoke tests for content-store, through api-gateway
    
    * Express api-gateway dependencies
    
    * Rename service to scholarly-articles_fpm
    
    * Use regex in location block of api-gateway
    
    * Remove stripping of trailing slashes
    
    * Add service to only existing fixture for scholarly-articles
    
    * Remove outdated options for location block
    
    * Reorder health waits by application
    
    * Separate backends and frontend check
    
    * Add /ping to api-gateway
    
    * Expose to host ports for all frontends
    
    * Add spec headers to /ping on api-gateway
    
    * Fix web localhost ports and block unknown host names
    
    * Match dummy-api data in scholarly-articles content-store
    
    * Standardize ports
    
    * Change healthcheck of api-gateway to use /ping
    
    * Move available /ping requests from smoke tests to healthchecks

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -3,12 +3,20 @@ set -ex
 
 COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-sample-configuration}"
 HTTP_PORT="${HTTP_PORT:-8080}"
+HTTP_PORT_GATEWAY="${HTTP_PORT_GATEWAY:-8081}"
 
 echo "Wait for containers health"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_scholarly-articles_fpm_1"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_scholarly-articles_web_1"
+.scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_api-gateway_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_dummy-api_fpm_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_browser_fpm_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_pattern-library_1"
 .scripts/docker/wait-healthy.sh "${COMPOSE_PROJECT_NAME}_web_1"
+
+echo "Smoke testing api-gateway (scholarly-articles content-store)"
+[[ "$(curl -sS "http://localhost:${HTTP_PORT_GATEWAY}/scholarly-articles/ping" 2>&1)" == "pong" ]]
+[[ "$(curl -sS "http://localhost:${HTTP_PORT_GATEWAY}/scholarly-articles/items" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
 
 echo "Smoke testing dummy-api"
 [[ "$(curl -sS -H 'Host: unstable--dummy-api.libero.pub' "http://localhost:${HTTP_PORT}/ping" 2>&1)" == "pong" ]]
