bd6b6bfcb2d518f165f853c035fb3a456190ec66
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-14 13:18:07 +0100

    Add script to remove volumes (#46)
    * Add script to remove volumes
    
    Containers need to be stopped for the volumes to be not in use and removable. Idempotent as it will only remove existing volumes.
    
    * Make listing of volumes not a script
    
    * Rename volumes list script
    
    * Break down external volumes script
    
    * Explain volume removal check
    
    * Explain intended use of external volumes script
    
    * Follow convention for exported variable
    
    * Substitute grep with equality check
    
    * Try shellcheck -x

--- .docker/initialize-volumes.sh
+++ .docker/initialize-volumes.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-volumes=()
-while IFS='' read -r line; do volumes+=("$line"); done < <(docker run --interactive --rm linkyard/yaml:1.1.0 /bin/bash -c "spruce json | jq --raw-output '.volumes | with_entries(select(.value.external == true )) | keys[]'" < docker-compose.yml)
+source .docker/.external-volumes.rc
 
-for volume in "${volumes[@]}"; do
+for volume in "${EXTERNAL_VOLUMES[@]}"; do
     docker volume create "${volume}"
 done
