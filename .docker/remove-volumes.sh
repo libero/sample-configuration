31a609387a8e6151545405c4e31d687f19846bb6
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-14 11:17:04 +0100

    Explain volume removal check

--- .docker/remove-volumes.sh
+++ .docker/remove-volumes.sh
@@ -4,7 +4,8 @@ set -e
 source .docker/.external-volumes.rc
 
 for volume in "${volumes[@]}"; do
-    if docker volume ls --filter name="${volume}" | grep "${volume}"; then
+    # if the volume is existing, grep matches it in the 1-item list
+    if docker volume ls --quiet --filter name="${volume}" | grep "${volume}"; then
         docker volume rm "${volume}"
     fi
 done
