5594cc14113835dd9054fda7574c147adf2247f9
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-27 14:31:58 +0100

    Use ENVIRONMENT_NAME in smoke tests

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -2,6 +2,7 @@
 set -ex
 
 COMPOSE_PROJECT_NAME="${COMPOSE_PROJECT_NAME:-sample-configuration}"
+ENVIRONMENT_NAME="${ENVIRONMENT_NAME:-unstable}"
 PUBLIC_PORT_HTTP="${PUBLIC_PORT_HTTP:-8080}"
 PUBLIC_PORT_HTTPS="${PUBLIC_PORT_HTTPS:-8443}"
 
@@ -33,11 +34,11 @@ done
 # HTTP locally
 https_configured="$(docker inspect "${COMPOSE_PROJECT_NAME}_web_1" | jq -r '.[0].NetworkSettings.Ports["443/tcp"]')"
 if [ "$https_configured" != "null" ]; then
-    jats_ingester="https://unstable--jats-ingester.libero.pub:${PUBLIC_PORT_HTTPS}"
-    api_gateway="https://unstable--api-gateway.libero.pub:${PUBLIC_PORT_HTTPS}"
-    dummy_api="https://unstable--dummy-api.libero.pub:${PUBLIC_PORT_HTTPS}"
-    browser="https://unstable.libero.pub:${PUBLIC_PORT_HTTPS}"
-    pattern_library="https://unstable--pattern-library.libero.pub:${PUBLIC_PORT_HTTPS}"
+    jats_ingester="https://${ENVIRONMENT_NAME}--jats-ingester.libero.pub:${PUBLIC_PORT_HTTPS}"
+    api_gateway="https://${ENVIRONMENT_NAME}--api-gateway.libero.pub:${PUBLIC_PORT_HTTPS}"
+    dummy_api="https://${ENVIRONMENT_NAME}--dummy-api.libero.pub:${PUBLIC_PORT_HTTPS}"
+    browser="https://${ENVIRONMENT_NAME}.libero.pub:${PUBLIC_PORT_HTTPS}"
+    pattern_library="https://${ENVIRONMENT_NAME}--pattern-library.libero.pub:${PUBLIC_PORT_HTTPS}"
 else
     jats_ingester="http://localhost:${PUBLIC_PORT_HTTP}"
     api_gateway="http://localhost:${PUBLIC_PORT_HTTP}"
@@ -47,28 +48,28 @@ else
 fi
 
 echo "Smoke testing jats-ingester"
-[[ "$(curl -sS -H "Host: unstable--jats-ingester.libero.pub" "${jats_ingester}/admin/" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--jats-ingester.libero.pub" "${jats_ingester}/admin/" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
 
 echo "Smoke testing api-gateway (blog-articles content-store)"
-[[ "$(curl -sS -H "Host: unstable--api-gateway.libero.pub" "${api_gateway}/blog-articles/ping" 2>&1)" == "pong" ]]
-[[ "$(curl -sS -H "Host: unstable--api-gateway.libero.pub" "${api_gateway}/blog-articles/items" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--api-gateway.libero.pub" "${api_gateway}/blog-articles/ping" 2>&1)" == "pong" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--api-gateway.libero.pub" "${api_gateway}/blog-articles/items" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
 
 echo "Smoke testing api-gateway (scholarly-articles content-store)"
-[[ "$(curl -sS -H "Host: unstable--api-gateway.libero.pub" "${api_gateway}/scholarly-articles/ping" 2>&1)" == "pong" ]]
-[[ "$(curl -sS -H "Host: unstable--api-gateway.libero.pub" "${api_gateway}/scholarly-articles/items" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--api-gateway.libero.pub" "${api_gateway}/scholarly-articles/ping" 2>&1)" == "pong" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--api-gateway.libero.pub" "${api_gateway}/scholarly-articles/items" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
 
 echo "Smoke testing api-gateway (search)"
-[[ "$(curl -sS -H "Host: unstable--api-gateway.libero.pub" "${api_gateway}/search/ping" 2>&1)" == "pong" ]]
-[[ "$(curl -sS -H "Host: unstable--api-gateway.libero.pub" "${api_gateway}/search" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--api-gateway.libero.pub" "${api_gateway}/search/ping" 2>&1)" == "pong" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--api-gateway.libero.pub" "${api_gateway}/search" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
 
 echo "Smoke testing dummy-api"
-[[ "$(curl -sS -H 'Host: unstable--dummy-api.libero.pub' "${dummy_api}/ping" 2>&1)" == "pong" ]]
-[[ "$(curl -sS -H 'Host: unstable--dummy-api.libero.pub' "${dummy_api}/blog-articles/ping" 2>&1)" == "pong" ]]
-[[ "$(curl -sS -H 'Host: unstable--dummy-api.libero.pub' "${dummy_api}/blog-articles/items" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--dummy-api.libero.pub" "${dummy_api}/ping" 2>&1)" == "pong" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--dummy-api.libero.pub" "${dummy_api}/blog-articles/ping" 2>&1)" == "pong" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}--dummy-api.libero.pub" "${dummy_api}/blog-articles/items" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
 
 echo "Smoke testing browser"
-[[ "$(curl -sS -H 'Host: unstable.libero.pub' "${browser}" --output /dev/null --write-out '%{http_code}')" == "200" ]]
-[[ "$(curl -sS --header 'Host: unstable.libero.pub' "${browser}/favicon.ico" --output /dev/null --write-out '%{http_code}')" == "200" ]]
+[[ "$(curl -sS -H "Host: ${ENVIRONMENT_NAME}.libero.pub" "${browser}" --output /dev/null --write-out '%{http_code}')" == "200" ]]
+[[ "$(curl -sS --header "Host: ${ENVIRONMENT_NAME}.libero.pub" "${browser}/favicon.ico" --output /dev/null --write-out '%{http_code}')" == "200" ]]
 
 echo "Smoke testing pattern library"
-[[ "$(curl -sS --header 'Host: unstable--pattern-library.libero.pub' "${pattern_library}" --output /dev/null --write-out '%{http_code}')" == "200" ]]
+[[ "$(curl -sS --header "Host: ${ENVIRONMENT_NAME}--pattern-library.libero.pub" "${pattern_library}" --output /dev/null --write-out '%{http_code}')" == "200" ]]
